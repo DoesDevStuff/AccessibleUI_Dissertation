700b29fb39b2d6ae9b752797c0a111fc1ef5cc27
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-06-09 00:34:58 +0100

    Shooting done
    created a bullet prefab, set in some shoot delays as well
    Made changes to the input manager (still on the old input system)
    bullet controller is created as well
    Player controller modified to handle shoot with rotation as well

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    #region Public properties
+    // change this if you want bullet visible for longer time
+    [SerializeField] float bullet_lifeTime = 0.5f;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(bullet_lifeTime);
+        Destroy(gameObject);
+
+    }
+}
