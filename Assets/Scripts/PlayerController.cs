f858bf4e5bb5f18c966c1e3628e4596eae9c01fc
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-06-08 05:08:19 +0100

    General scene set up with movement and collision
    Art assets
    Also set up player character
    moves and collides with collectibles to which a counter is added for now

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public static int amountCollected = 0;
+
+    [SerializeField] float playerSpeed = 5;
+    [SerializeField] Text collectedText;
+    
+    Rigidbody2D player_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        player_rigidbody.velocity = new Vector3(horizontal * playerSpeed, vertical * playerSpeed, 0);
+        collectedText.text = "Items Collected: " + amountCollected;
+    }
+}
