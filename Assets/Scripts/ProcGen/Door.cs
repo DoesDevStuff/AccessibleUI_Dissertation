77516ebfd224a0118b4725b86249dfaf04ba9dd6
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-06-24 06:02:55 +0100

    Art cleaned up and moved to diff folders
    Added a new folder to hold finalised assets
    Also added new scripts for Proc gen

---
+++ Assets/Scripts/ProcGen/Door.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public enum DoorType
+    {
+        left, right, top, bottom
+    }
+
+    public DoorType doorType;
+
+    public GameObject doorCollider;
+
+    private GameObject _player;
+    private float _widthOffset = 4f;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            switch (doorType)
+            {
+                case DoorType.bottom:
+                    _player.transform.position = new Vector2(transform.position.x, transform.position.y - _widthOffset);
+                    break;
+                case DoorType.left:
+                    _player.transform.position = new Vector2(transform.position.x - _widthOffset, transform.position.y);
+                    break;
+                case DoorType.right:
+                    _player.transform.position = new Vector2(transform.position.x + _widthOffset, transform.position.y);
+                    break;
+                case DoorType.top:
+                    _player.transform.position = new Vector2(transform.position.x, transform.position.y + _widthOffset);
+                    break;
+            }
+        }
+    }
+}
