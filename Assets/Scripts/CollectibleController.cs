c60ad4119d712bcee807e75aff6b280085e486e9
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-06-24 03:49:39 +0100

    This is a recloned and relinked branch
    Relinked all the assets after cloning and reattaching stuff

--- Assets/Scripts/CollectibleController.cs
+++ Assets/Scripts/CollectibleController.cs
@@ -2,25 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public string description;
+    public Sprite itemImage;
+}
+
 public class CollectibleController : MonoBehaviour
 {
+    #region Public properties
+    public Item item;
+    public float healthChange;
+    public float moveSpeedChange;
+    public float attackSpeedChange;
+    public float bulletSizeChange;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().sprite = item.itemImage;
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             PlayerController.amountCollected++;
+            GameController.HealPlayer(healthChange);
+            GameController.MoveSpeedChange(moveSpeedChange);
+            GameController.FireRateChange(attackSpeedChange);
+            GameController.BulletSizeChange(bulletSizeChange);
+           // GameController.instance.UpdateCollectedItems(this);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,29 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public static int amountCollected = 0;
-
+    
+    #region Public properties
+    [Header("Player Properties")]
     [SerializeField] float playerSpeed = 5;
     [SerializeField] Text collectedText;
-    
+
     Rigidbody2D player_rigidbody;
+
+    [Header("Bullet Properties")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletSpeed = 5.5f;
+    [SerializeField] float firingDelay = 0.5f;
+    #endregion
+
+    #region Private properties
+    [Header("Bullet Properties")]
+    private float _lastFired;
+
+    #endregion
+
+    [Header("Collectibles")]
+    public static int amountCollected = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +38,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        // player movement inputs
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // shoot inputs
+        float shootHorizontal = Input.GetAxis("ShootHorizontal");
+        float shootVertical = Input.GetAxis("ShootVertical");
+
+        if((shootHorizontal != 0 || shootVertical != 0) && Time.time > _lastFired + firingDelay)
+        {
+            Shoot(shootHorizontal, shootVertical);
+            _lastFired = Time.time;
+        }
+
         player_rigidbody.velocity = new Vector3(horizontal * playerSpeed, vertical * playerSpeed, 0);
         collectedText.text = "Items Collected: " + amountCollected;
+
+
+        // item system updates
+        firingDelay = GameController.FireRate;
+        playerSpeed = GameController.MoveSpeed;
+
+    }
+
+    // method to handle Shooting
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,    
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,    
+            0
+        );
     }
 }
