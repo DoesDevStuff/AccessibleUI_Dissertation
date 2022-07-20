c85a27f7e57860cc5c40cff125d9c389c98cb7b9
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-07-20 17:36:51 +0100

    Slider system with colour changed basic
    Set up a colour picker to change colour of character in the menu scene.
    Scene has a slider script and a scene manager as well a player script to handle reflection of chosen colour onto the player

---
+++ Assets/Scripts/UI/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Color GetColor()
+    {
+        return GetComponent<SpriteRenderer>().color;
+    }
+
+    public void SetColor(Color newColor)
+    {
+        GetComponent<SpriteRenderer>().color = newColor;
+    }
+}
---
+++ Assets/Scripts/UI/SliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    private Slider slider;
+    public int colorIndex = 0;
+    private SceneScript sceneScript;
+
+    void Start()
+    {
+        sceneScript = GameObject.Find("SceneManager").GetComponent<SceneScript>();
+        slider = gameObject.GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    public void ValueChangeCheck()
+    {
+        sceneScript.ChangePlayerColor(colorIndex, slider.value);
+    }
+}
