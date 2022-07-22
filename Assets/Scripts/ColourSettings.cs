39ea44f5bcedcc781d945f64f45baa3b9ac57f13
Marcus <Hello@MarcusSkov.com>
2022-07-22 14:51:06 +0100

    # Initial Color Settings - [Added] Color theme system to the game via scriptable objects - [Added] ColorSettings controller to BasementMain scene

---
+++ Assets/Scripts/ColourSettings.cs
@@ -0,0 +1,56 @@
+﻿using NaughtyAttributes;
+using ScriptableObjects;
+using UnityEngine;
+
+public class ColourSettings : MonoBehaviour
+{
+    // Character Materials
+    [SerializeField] private Material playerMaterial;
+    [SerializeField] private Material enemyMaterial;
+    
+    // Items Materials
+    [SerializeField] private Material itemsHealthMaterial;
+    [SerializeField] private Material itemsSpeedBuffMaterial;
+    [SerializeField] private Material itemsAttackBuffMaterial;
+    
+    // Environment Materials
+    [SerializeField] private Material doorMaterials;
+    [SerializeField] private Material wallMaterial;
+
+    [SerializeField] private ColorThemes[] colorthemes;
+    [Range(0, 3)]
+    [SerializeField] private int activeTheme;
+
+    private void UpdateMaterial(Material material, Color newColour)
+    {
+        material.color = newColour;
+    }
+
+    [Button]
+    private void ChangeColourTheme()
+    {
+        // TODO - Replace with stored colour theme
+        playerMaterial.color = colorthemes[activeTheme].playerColor;
+        enemyMaterial.color = colorthemes[activeTheme].enemyColor;
+        
+        itemsHealthMaterial.color = colorthemes[activeTheme].itemsHealthColor;
+        itemsSpeedBuffMaterial.color = colorthemes[activeTheme].itemsSpeedBuffColor;
+        itemsAttackBuffMaterial.color = colorthemes[activeTheme].itemsAttackBuffColor;
+
+        doorMaterials.color = colorthemes[activeTheme].doorColor;
+        wallMaterial.color = colorthemes[activeTheme].wallColor;
+    }
+    
+    private void ResetMaterials()
+    {
+        playerMaterial.color = colorthemes[activeTheme].playerColor;
+        enemyMaterial.color = colorthemes[activeTheme].enemyColor;
+        
+        itemsHealthMaterial.color = colorthemes[activeTheme].itemsHealthColor;
+        itemsSpeedBuffMaterial.color = colorthemes[activeTheme].itemsSpeedBuffColor;
+        itemsAttackBuffMaterial.color = colorthemes[activeTheme].itemsAttackBuffColor;
+
+        doorMaterials.color = colorthemes[activeTheme].doorColor;
+        wallMaterial.color = colorthemes[activeTheme].wallColor;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/ColorThemes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using static UnityEngine.Color;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "ColorTheme.asset", menuName = "ColorTheme")]
+    public class ColorThemes : ScriptableObject
+    {
+        public enum ColorThemesEnum
+        {
+            Default,
+            Deuteranomaly,
+            Trichromacy,
+            Monochromacy
+        }
+
+        public ColorThemesEnum colorTheme;
+    
+        public Color playerColor = white;
+        public Color enemyColor = white;
+        public Color itemsHealthColor = white;
+        public Color itemsSpeedBuffColor = white;
+        public Color itemsAttackBuffColor = white;
+        public Color doorColor = white;
+        public Color wallColor = white;
+    }
+}
