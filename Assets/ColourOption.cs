af194c7870cd6783adde322da51e75fa40be94a3
Marcus <Hello@MarcusSkov.com>
2022-07-27 20:56:09 +0100

    Added initial Setting menu

---
+++ Assets/ColourOption.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourOption : MonoBehaviour
+{
+    [SerializeField] private Button colourButton;
+    [SerializeField] private Button resetButton;
+
+
+    private void Start()
+    {
+        colourButton.onClick.AddListener( () => openColourWheel());
+        resetButton.onClick.AddListener( () => resetColour());
+    }
+    
+    private void openColourWheel()
+    {
+        print("Open colour wheel");
+        
+        // TODO - Save Colours to persist across play instances
+        // TODO - Change colour of the button
+        // TODO - Change colour of the game prefabs
+    }
+    
+    private void resetColour()
+    {
+        // TODO - Reset colour and revert the above to the initial colours    
+    }
+    
+}
