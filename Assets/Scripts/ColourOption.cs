0aa9c9015093ccbd90ea8eef3d28535a6f146026
Marcus <Hello@MarcusSkov.com>
2022-07-31 20:03:54 +0100

    Added state for when colour wheel should appear - [Added] Settings Manager script - [Added] Disabled state for scrollbar when colour wheel should be present

---
+++ Assets/Scripts/ColourOption.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ColourOption : MonoBehaviour
+{
+    [SerializeField] private Button colourButton;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private TMP_Text optionText;
+
+    private void Start()
+    {
+        colourButton.onClick.AddListener(ColourOptionClick);
+        resetButton.onClick.AddListener(ResetColour);
+    }
+    
+    private void ColourOptionClick()
+    {
+        if (SettingsManager.currentColorOption == null)
+        {
+            print("Opening a colour wheel");
+            OpenColourWheel();
+        }
+        else if (SettingsManager.currentColorOption != this)
+        {
+            print("Selecting another colour option while another is already open");
+        }
+        else
+        {
+            print("Selecting the same colour option");
+        }
+    }
+
+    private void OpenColourWheel()
+    {
+        // Sets the current colour options button as the game object in the settings manager
+        SettingsManager.currentColorOption = null;
+        SettingsManager.EnableFullscreenColourWheelCloseButton(this);
+        
+        colourButton.onClick.RemoveAllListeners();
+        colourButton.onClick.AddListener(CloseColourWheel);
+
+        transform.GetComponent<Image>().color = Color.black;
+        optionText.color = Color.white;
+        
+        // TODO - Save Colours to persist across play instances
+        // TODO - Change colour of the game prefabs 
+    }
+
+    public void CloseColourWheel()
+    {
+        SettingsManager.currentColorOption = null;
+        SettingsManager.DisableFullscreenColourWheelCloseButton();
+
+        transform.GetComponent<Image>().color = Color.clear;
+        optionText.color = Color.black;
+        
+        colourButton.onClick.AddListener(ColourOptionClick);
+    }
+    
+    private void ResetColour()
+    {
+        // TODO - Reset colour and revert the above to the initial colours    
+    }
+
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static ColourOption currentColorOption;
+    
+    private static Button _closeColourOptionButton;
+    private static ScrollRect _scrollRect;
+    private static Scrollbar _scrollbar; 
+
+    public void Awake()
+    {
+        _closeColourOptionButton = transform.GetChild(0).GetChild(2).GetComponent<Button>();
+        _scrollRect = transform.GetChild(0).GetChild(3).GetComponent<ScrollRect>();
+        _scrollbar = _scrollRect.transform.GetChild(1).GetComponent<Scrollbar>();
+    }
+
+    public static void EnableFullscreenColourWheelCloseButton(ColourOption activeColorOption)
+    {
+        currentColorOption = activeColorOption;
+        _closeColourOptionButton.onClick.AddListener(currentColorOption.CloseColourWheel);
+        _closeColourOptionButton.GetComponent<Image>().raycastTarget = true;
+
+        // Disables scrolling after having opened the colour picker - also enables closure of the colour picker by
+        // pressing anywhere on the UI canvas
+        DisableSettingsScrolling();
+    }
+
+    public static void DisableFullscreenColourWheelCloseButton()
+    {
+        _closeColourOptionButton.GetComponent<Image>().raycastTarget = false;
+        _closeColourOptionButton.onClick.RemoveAllListeners();
+        
+        EnableSettingsScrolling();
+    }
+    
+    private static void DisableSettingsScrolling()
+    {
+        _scrollRect.enabled = false;
+        _scrollbar.interactable = false;
+    }
+
+    public static void EnableSettingsScrolling()
+    {
+        _scrollRect.enabled = true;
+        _scrollbar.interactable = true;
+        
+    }
+
+}
