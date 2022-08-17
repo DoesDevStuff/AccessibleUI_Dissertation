9847044070ad1a8f1054c5ab4be9c071346a6c7f
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-08-17 20:06:16 +0100

    Added scripts to toggle Menu and pause screens

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This assumes we have a plain page for Menu as a basic separate scene
+//And that options / settings menu / pause menu are all separate canvas
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    public GameObject optionsScreen;
+    
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    /// <summary>
+    /// Loads Options Menu
+    /// </summary>
+    public void OpenOptions()//opens options menu
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()//closes and sets canvas back to invisible
+    {
+        optionsScreen.SetActive(false);
+    }
+    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f; // freeze the game scene
+
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ReturnToMain()
+    {
+        Time.timeScale = 1f; // set timescale back to 1 or normal
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
